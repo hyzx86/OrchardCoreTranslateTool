f81820975a60bab8f49d9612b840e057457103b2
Tony Han <huarui2219@qq.com>
2019-01-21 14:11:18 +0800

    add merge crowdin fies tool

---
+++ MergCrowdinFiles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergCrowdinFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string newFileName = "zh-CN.po";
+
+            var files = Directory.EnumerateFiles(@"C:\Users\hyzx8\Documents\Visual Studio 2017\Projects\OrchardCoreTranslateTool\MergCrowdinFiles\zh-CN"
+             , "*.po", System.IO.SearchOption.AllDirectories);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var item in files)
+            {
+                stringBuilder.Append(File.ReadAllText(item));
+                stringBuilder.AppendLine("# From File:" + item);
+            }
+
+            File.WriteAllText(newFileName, stringBuilder.ToString());
+        }
+    }
+}
